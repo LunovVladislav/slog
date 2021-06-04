f233226090fa7a8f01cd3db286f68f0b54c111e8
Vladislav Lunov <LunovVladislav@users.noreply.github.com>
2021-06-04 15:29:17 +0300

    - exception

--- log/__init__.py
+++ log/__init__.py
@@ -1,3 +1,4 @@
+from sys import exit
 from datetime import datetime
 
 class Log:
@@ -21,6 +22,12 @@ class Log:
 		self.log("error", message)
 
 	def log(self, mode, message):
+		if "-" in message:
+			print("\033[1;31;40mError: message can't contain \"-\"")
+			exit(1)
+		if "-" in mode:
+			print("\033[1;31;40mError: tag can't contain \"-\"")
+			exit(1)
 		file = open(self.fileName, "a")
 		file.write(mode+"-"+message+"-"+self.time()+"\n")
 		file.close()
